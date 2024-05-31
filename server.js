a7338a501b1741fea139321e14843cc3f7b2d60a
Antoine-Alexandr Leytens <aa.leytens@gmail.com>
2024-05-31 19:25:41 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
-// Définir le chemin vers le dossier de distribution
-const distPath = path.join(__dirname, "dist", "portfolio");
-
-// Servir les fichiers statiques depuis le dossier de distribution
-app.use(express.static(distPath));
-
-// Route pour servir l'index.html
+app.use(express.static(__dirname + "/dist/<app-name>"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.resolve(distPath, "index.html"));
+  res.sendFile(path.join(__dirname + "/dist/<app-name>/index.html"));
 });
-
-// Démarrer le serveur
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 8080);
